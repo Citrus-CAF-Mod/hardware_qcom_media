14d5761b2ea51cde9b01233c0b379c57ee1ed860
Suman Mukherjee <sumam@codeaurora.org>
2017-07-06 16:01:18 +0530

    hal: Added LOCAL_VENDOR_MODULE to set output path of the binaries
    Replaced LOCAL_PROPRIETARY_MODULE with LOCAL_VENDOR_MODULE to set the
    output vendor path for hal binaries and libraries
    
    Change-Id: Ib04d80eabc0e17f3863e956db5f2378e426c0687
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- mm-video-v4l2/vidc/vdec/Android.mk
+++ mm-video-v4l2/vidc/vdec/Android.mk
@@ -107,7 +107,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE                    := libOmxVdec
 LOCAL_MODULE_TAGS               := optional
 LOCAL_CFLAGS                    := $(libmm-vdec-def) -Werror -Wno-error
-LOCAL_PROPRIETARY_MODULE        := true
+LOCAL_VENDOR_MODULE             := true
 LOCAL_C_INCLUDES                += $(libmm-vdec-inc)
 LOCAL_ADDITIONAL_DEPENDENCIES   := $(libmm-vdec-add-dep)
 
@@ -142,7 +142,7 @@ ifeq ($(call is-board-platform-in-list, $(TARGETS_THAT_NEED_HEVC_LIB)),true)
 
 LOCAL_MODULE                    := libOmxVdecHevc
 LOCAL_MODULE_TAGS               := optional
-LOCAL_PROPRIETARY_MODULE        := true
+LOCAL_VENDOR_MODULE             := true
 LOCAL_CFLAGS                    := $(libmm-vdec-def)
 LOCAL_C_INCLUDES                += $(libmm-vdec-inc)
 LOCAL_ADDITIONAL_DEPENDENCIES   := $(libmm-vdec-add-dep)
